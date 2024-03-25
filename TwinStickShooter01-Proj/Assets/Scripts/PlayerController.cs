cad7e0d076f33fe616c8b7755233d92432795a99
RSkala <RichardSkala@gmail.com>
2024-03-24 18:20:34 -0700

    First pass gun rotation

--- TwinStickShooter01-Proj/Assets/Scripts/PlayerController.cs
+++ TwinStickShooter01-Proj/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _moveSpeed;
+    [SerializeField] Transform _gunRotationPoint;
     Vector2 _moveInput;
+    Vector2 _lookInput;
     Rigidbody2D _rigidBody2D;
 
     void Start()
@@ -22,6 +24,15 @@ public class PlayerController : MonoBehaviour
             Vector2 newPosition = _rigidBody2D.position + movementDirection * _moveSpeed * Time.fixedDeltaTime;
             _rigidBody2D.MovePosition(newPosition);
         }
+
+        // Update Look
+        if(!_lookInput.Equals(Vector2.zero))
+        {
+            Vector3 cross = Vector3.Cross(Vector2.up, _lookInput);
+            float flipValue = cross.z < 0.0f ? -1.0f : 1.0f;
+            float rotateAngle = Vector2.Angle(Vector2.up, _lookInput) * flipValue;
+            _gunRotationPoint.rotation = Quaternion.Euler(0.0f, 0.0f, rotateAngle);  
+        }
     }
 
     void OnMove(InputValue inputValue)
@@ -31,7 +42,7 @@ public class PlayerController : MonoBehaviour
 
     void OnLook(InputValue inputValue)
     {
-        //Debug.Log("OnLook - " + gameObject.name);
+        _lookInput = inputValue.Get<Vector2>();
     }
 
     void OnFire(InputValue inputValue)
